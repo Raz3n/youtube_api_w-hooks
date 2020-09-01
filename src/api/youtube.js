a9c0e97f4060d22615885855e3f9a134a09895e0
Nelson <nbritob@hotmail.com>
2020-09-01 11:06:10 +0100

    adds axios

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,1 +1,12 @@
-const Key = "AIzaSyDbFC9usQz3cxNLUWPaGWF-Y_cRf_U02O8"
+import axios from "axios"
+const KEY = "AIzaSyDbFC9usQz3cxNLUWPaGWF-Y_cRf_U02O8"
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+      part: 'snippet', 
+      type: 'video',
+      maxResults: 5,
+      key: KEY
+  }
+})
