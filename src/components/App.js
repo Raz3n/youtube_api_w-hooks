4a0add88da740f75473fa5df958976f6f1aacd90
Nelson <nbritob@hotmail.com>
2020-09-02 14:58:53 +0100

    adds onVideoSelect state which is passed down to videolist and videoitem

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,10 @@ import VideoList from "./VideoList";
 import youtube from "../api/youtube";
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = {
+    videos: [],
+    selectedVideo: null,
+  };
 
   onTermSubmit = async (term) => {
     const response = await youtube.get("/search", {
@@ -14,11 +17,16 @@ class App extends React.Component {
     });
     this.setState({ videos: response.data.items });
   };
+
+  onVideoSelect = (video) => {
+    console.log('from the app',video)
+  }
+
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
       </div>
     );
   }
