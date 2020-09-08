791de8decbf41fe3fa9f1e9a83ab8fb55c4da1ac
Nelson <nbritob@hotmail.com>
2020-09-08 13:46:36 +0100

    refactors App to include custom hook and finishes app

--- src/components/App.js
+++ src/components/App.js
@@ -2,28 +2,28 @@ import React, { useState, useEffect } from "react";
 import SearchBar from "./SearchBar";
 import VideoList from "./VideoList";
 import VideoDetail from "./VideoDetail";
-import youtube from "../api/youtube";
+import useVideos from "../hooks/useVideos";
 
 const App = () => {
   const [selectedVideo, setSelectedVideo] = useState(null);
+  const [videos, search] = useVideos("shakira");
+
+  useEffect(() => {
+    setSelectedVideo(videos[0]);
+  }, [videos]);
 
   // setSelectedVideo(response.data.items[0]);
 
-
-  
-  
   return (
     <div className="ui container">
-      <SearchBar onFormSubmit={onTermSubmit} />
+      <SearchBar onFormSubmit={search} />
       <div className="ui grid">
         <div className="ui row">
           <div className="eleven wide column">
             <VideoDetail video={selectedVideo} />
           </div>
           <div className="five wide column">
-            <VideoList
-             onVideoSelect={setSelectedVideo} 
-             videos={videos} />
+            <VideoList onVideoSelect={setSelectedVideo} videos={videos} />
           </div>
         </div>
       </div>
